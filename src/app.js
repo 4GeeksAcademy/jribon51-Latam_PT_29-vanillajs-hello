525520d45672d6c9a5d57af9cf77cc77ef236392
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-03-12 02:32:12 +0000

    [subida cimpleta del ejercicio de excusas] se modifica el html al que se le agrega solo el id y se modifica el archivo js en el cual se describe la funcion primero generando la frase por medio de numeros aleattorios y despues se trae el elemento por medio del id para cambiar el texto por medio de la propiedad innertext

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let frase =
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    action[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 3)] +
+    " " +
+    when[Math.floor(Math.random() * 5)];
+
+  let variable = document.getElementById("excuse");
+  variable.innerText = frase;
 };
